97db75aa624b17c9625ee03794d6c73bd150c2b8
oboril <jan.oboril@gmail.com>
2022-06-25 19:27:05 -0400

    giving this shit anothor chagewnae

---
+++ Encryption/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import os;
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/setup")
+def setup():
+  
