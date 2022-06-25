bce929a43273fecaccd046b238271634cbc502a8
oboril <jan.oboril@gmail.com>
2022-06-25 19:34:57 -0400

    more update on python

--- Encryption/app.py
+++ Encryption/app.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+  return render_template("index.html")
 
 @app.route("/setup")
 def setup():
-  
+  private_key = "0x952b1d76efd035559bedd3f748d8ef737fc87fdc7efc66285b3fc4770910008f"
+  os.system(f"cd ../BackendContract && brownie run setup setup {private_key} --network rinkeby")
+
+  return "Response from python"
