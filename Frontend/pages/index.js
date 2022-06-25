f29f9a6ec6388529359e8655b966faf899c74a4f
Kalina <532anilak@gmail.com>
2022-06-25 13:06:29 +0100

    imports

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -1,9 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Contacts from '../components/Contacts'
+import Chat from '../components/Chat'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
+    
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
