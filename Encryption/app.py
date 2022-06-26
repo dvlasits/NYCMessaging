c9c3d90ee054202fb5796fdba73f547aa5f57803
oboril <jan.oboril@gmail.com>
2022-06-25 20:00:06 -0400

    another ewbsite changes

--- Encryption/app.py
+++ Encryption/app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
-import os;
+from flask import Flask, render_template, request
+import os
+import json
 
 app = Flask(__name__)
+
+private_key = "0x952b1d76efd035559bedd3f748d8ef737fc87fdc7efc66285b3fc4770910008f"
 
 @app.route("/")
 def index():
@@ -9,7 +12,17 @@ def index():
 
 @app.route("/setup")
 def setup():
-  private_key = "0x952b1d76efd035559bedd3f748d8ef737fc87fdc7efc66285b3fc4770910008f"
   os.system(f"cd ../BackendContract && brownie run setup setup {private_key} --network rinkeby")
 
-  return "Response from python"
+  return "Response from python /setup"
+
+@app.route("/set_con", methods=['GET','POST'])
+def set_con():
+  data = json.loads(request.data)
+  target_address = data['target_address']
+
+  print(target_address)
+
+  os.system(f"cd ../BackendContract && brownie run setConn addObjToTalk {target_address} {private_key} --network rinkeby")
+
+  return "Response from python /set_con"
