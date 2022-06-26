0826285b6d6ba43f66097c6a9caa0feb90a1eff2
oboril <jan.oboril@gmail.com>
2022-06-25 21:17:33 -0400

    added stuff to app.py

--- Encryption/app.py
+++ Encryption/app.py
@@ -5,6 +5,7 @@ import json
 app = Flask(__name__)
 
 private_key = "0x952b1d76efd035559bedd3f748d8ef737fc87fdc7efc66285b3fc4770910008f"
+wallet_address = "0x66d9fb0Cd2D9103C29fF3a695479875FA422F6A8"
 
 @app.route("/")
 def index():
@@ -20,17 +21,25 @@ def setup():
 def set_con():
   data = json.loads(request.data)
   target_address = data['target_address']
+  message = data['message'].replace(' ', '_')
 
   print(target_address)
 
-  print(f"cd ../BackendContract && brownie run setConn addObjToTalk {target_address} {private_key} --network rinkeby")
-  os.system(f"cd ../BackendContract && brownie run setConn addObjToTalk {target_address} {private_key} --network rinkeby")
+
+  print(f"cd ../BackendContract && brownie run setConn addObjToTalk {target_address} {message} {private_key} --network rinkeby")
+  os.system(f"cd ../BackendContract && brownie run setConn addObjToTalk {target_address} {message} {private_key} --network rinkeby")
 
   return "Response from python /set_con"
 
 
 @app.route("/get_con")
-def setup():
+def get_con():
   os.system(f"cd ../BackendContract && brownie run recConn recConnec {private_key} --network rinkeby")
 
-  return "Response from python /setup"
+  return "Response from python /get_con"
+
+@app.route("/read_message")
+def read_message():
+  os.system(f"cd ../BackendContract && brownie run writeMessage readMessage {wallet_address} --network rinkeby")
+
+  return "Response from python /read_message"
