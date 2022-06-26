e9598f619ec8f46a1bedbb596d68f78160fdf434
oboril <jan.oboril@gmail.com>
2022-06-26 00:52:59 -0400

    added variable networks

--- Encryption/app.py
+++ Encryption/app.py
@@ -8,13 +8,15 @@ app = Flask(__name__)
 private_key = "a65c77708018bc98235d266267fed947f87d87405a36f2573ec2e4f37d49fe9e"#"0x952b1d76efd035559bedd3f748d8ef737fc87fdc7efc66285b3fc4770910008f"
 wallet_address = "0x61fF6367537Ab46FccAf06bB9FD05A1b308fb7B6"#"0x66d9fb0Cd2D9103C29fF3a695479875FA422F6A8"
 
+network = "rinkeby" #"boba-test"
+
 @app.route("/")
 def index():
   return render_template("index.html")
 
 @app.route("/setup")
 def setup():
-  os.system(f"cd ../BackendContract && brownie run setup setup {private_key} --network rinkeby")
+  os.system(f"cd ../BackendContract && brownie run setup setup {private_key} --network {network}")
 
   return "Response from python /setup"
 
@@ -29,8 +31,8 @@ def set_con():
   print(target_address)
   print(f'message: {message}')
 
-  print(f"cd ../BackendContract && brownie run setConn addObjToTalk {target_address} {message} {private_key} --network rinkeby")
-  os.system(f"cd ../BackendContract && brownie run setConn addObjToTalk {target_address} {message} {private_key} --network rinkeby")
+  print(f"cd ../BackendContract && brownie run setConn addObjToTalk {target_address} {message} {private_key} --network {network}")
+  os.system(f"cd ../BackendContract && brownie run setConn addObjToTalk {target_address} {message} {private_key} --network {network}")
 
   return "Response from python /set_con"
 
@@ -41,8 +43,8 @@ def get_con():
   target_address = data['target_address']
   print(target_address)
 
-  os.system(f"cd ../BackendContract && brownie run recConn recConnec {private_key} --network rinkeby")
-  os.system(f"cd ../BackendContract && brownie run writeMessage readMessage {target_address} --network rinkeby")
+  os.system(f"cd ../BackendContract && brownie run recConn recConnec {private_key} --network {network}")
+  os.system(f"cd ../BackendContract && brownie run writeMessage readMessage {target_address} --network {network}")
 
   with open("../BackendContract/textOut.json") as f:
     data = json.load(f)
@@ -55,6 +57,6 @@ def get_con():
 
 @app.route("/read_message")
 def read_message():
-  os.system(f"cd ../BackendContract && brownie run writeMessage readMessage {wallet_address} --network rinkeby")
+  os.system(f"cd ../BackendContract && brownie run writeMessage readMessage {wallet_address} --network {network}")
 
   return "Response from python /read_message"
